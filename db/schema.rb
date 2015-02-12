4881be6051b8861eb400e7beac8254b199b5ca64
mike vallano <mikevallano@gmail.com>
2015-02-12 09:44:07 -0600

    synced users and todos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212144102) do
+ActiveRecord::Schema.define(version: 20150212152109) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150212144102) do
     t.boolean  "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
